72fb23901833718c391d2c8ca20702109a211e45
Shun <118155300+Fujimaru554264106@users.noreply.github.com>
2022-11-15 21:50:49 +0700

    Update IngredientDAO.java

--- src/cookingNotebook/DAO/IngredientDAO.java
+++ src/cookingNotebook/DAO/IngredientDAO.java
@@ -6,5 +6,6 @@ import java.util.List;
 public interface IngredientDAO extends DAO<Ingredient> {
 
 	public List<Ingredient> getIngredientList(int fid) throws SQLException;
+	public Ingredient getIngredient(String name) throws SQLException;
 	
 }
