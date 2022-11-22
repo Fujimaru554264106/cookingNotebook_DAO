a4b70e80cffbce59f5b13573b9508f2664df39ed
Shun <118155300+Fujimaru554264106@users.noreply.github.com>
2022-11-22 13:11:09 +0700

    Delete IngredientDAO.java

--- src/cookingNotebook/DAO_02/IngredientDAO.java
+++
@@ -1,9 +0,0 @@
-package cookingNotebook.DAO_02;
-
-import java.sql.SQLException;
-
-public interface IngredientDAO extends DAO<Ingredient> {
-
-	public Ingredient get(String s) throws SQLException;
-	
-}
